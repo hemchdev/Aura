6eb577bc77066a3cdd213b582ae179459d3c9f1e
hemchdev <hemanthsai1566@gmail.com>
2025-07-03 09:48:10 +0530

    Manifest fix

--- plugins/withAndroidManifestFix.js
+++ plugins/withAndroidManifestFix.js
@@ -4,17 +4,29 @@ const withAndroidManifestFix = (config) => {
   return withAndroidManifest(config, (config) => {
     const androidManifest = config.modResults;
     
-    // Fix manifest merger conflicts
-    if (androidManifest.manifest.application) {
-      // Add tools:replace for appComponentFactory
-      androidManifest.manifest.application.$['android:appComponentFactory'] = 'androidx.core.app.CoreComponentFactory';
-      androidManifest.manifest.application.$['tools:replace'] = 'android:appComponentFactory';
-      
-      // Ensure xmlns:tools is added to manifest
-      if (!androidManifest.manifest.$['xmlns:tools']) {
-        androidManifest.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
-      }
+    // Ensure the manifest structure exists
+    if (!androidManifest.manifest) {
+      androidManifest.manifest = {};
     }
+    
+    if (!androidManifest.manifest.$) {
+      androidManifest.manifest.$ = {};
+    }
+    
+    if (!androidManifest.manifest.application) {
+      androidManifest.manifest.application = [{}];
+    }
+    
+    if (!androidManifest.manifest.application[0].$) {
+      androidManifest.manifest.application[0].$ = {};
+    }
+    
+    // Add tools namespace to manifest root
+    androidManifest.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
+    
+    // Fix manifest merger conflicts by adding tools:replace
+    androidManifest.manifest.application[0].$['android:appComponentFactory'] = 'androidx.core.app.CoreComponentFactory';
+    androidManifest.manifest.application[0].$['tools:replace'] = 'android:appComponentFactory';
     
     return config;
   });
