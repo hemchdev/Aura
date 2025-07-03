3c9bb2c171ebee3e55860ef9995b84d30a79fa8f
hemchdev <hemanthsai1566@gmail.com>
2025-07-03 14:50:04 +0530

    android folder

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add resolver configuration to help with dependency resolution
+config.resolver.blacklistRE = /android\/.*|ios\/.*/;
+
 module.exports = config;
